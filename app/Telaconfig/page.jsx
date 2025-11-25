75bba6c3baf7c80221a9e621bf9585d5ca28705c
eliastk0 <elias.neto3755@gmail.com>
2025-11-25 20:41:04 -0300

    adicionei a tela config completa

--- app/Telaconfig/page.jsx
+++ app/Telaconfig/page.jsx
@@ -13,8 +13,9 @@ export default function Telaconfig() {
       <Navbar />
 
       <div className="bg-[#7c3aed] min-h-screen pt-20 px-6 flex justify-center items-start">
-        <div className="bg-white w-[95%] max-w-4xl rounded-3xl shadow-xl p-14 relative mx-auto">
-          <button className="absolute left-8 top-8">
+        <div className="bg-white w-[95%] max-w-4xl mt-3 rounded-3xl shadow-xl p-28 relative mx-auto">
+          <Link href="./" className="w-full block">
+          <button className="absolute left-8 top-8 ">
             <svg
               width="30"
               height="30"
@@ -27,6 +28,7 @@ export default function Telaconfig() {
               <path d="M18 22L10 14L18 6" />
             </svg>
           </button>
+          </Link>
           <div className="flex flex-col items-center -mt-12 mb-12">
             <Image
               src="/images/lele.png"
@@ -35,33 +37,33 @@ export default function Telaconfig() {
               width={120}
               height={120}
             />
-            <Button className="mt-6 bg-[#7c3aed] px-8 py-2 rounded-full shadow text-white">
+            <Button className="mt-6 bg-[#7c3aed]  hover:bg-purple-800 px-8 py-2 rounded-full shadow text-white">
               Alterar
             </Button>
           </div>
-          <div className="bg-gray-300 rounded-full px-8 py-6 flex items-center justify-between mb-8">
+          <div className="bg-gray-300 rounded-full px-6 py-4 flex items-center justify-between mb-8">
             <div>
-              <Label className="font-semibold text-gray-800">
+              <Label className="font-semibold text-gray-800 text-[18px]">
                 Alterar nome de usuário
               </Label>
-              <p className="text-sm text-gray-700 mt-1">Bruno</p>
+              <p className="text-sm text-gray-700 mt-1 text-[17px]">Bruno</p>
             </div>
 
             <Link href="/Telaconfig/Nome">
-              <Button className="bg-[#7c3aed] text-white px-6 py-2 rounded-full shadow">
+              <Button className="bg-[#7c3aed] text-white  hover:bg-purple-800 px-6 py-2 rounded-full shadow">
                 Alterar
               </Button>
             </Link>
           </div>
-          <div className="bg-gray-300 rounded-full px-8 py-6 flex items-center justify-between">
+          <div className="bg-gray-300 rounded-full px-6 py-4 flex items-center justify-between">
             <div>
-              <Label className="font-semibold text-gray-800">
+              <Label className="font-semibold text-gray-800 text-[18px]">
                 Alterar Senha
               </Label>
-              <p className="text-sm text-gray-700 mt-1">************</p>
+              <p className="text-sm text-gray-700 mt-1 text-[17px]">************</p>
             </div>
             <Link href="/Telaconfig/Senha">
-              <Button className="bg-[#7c3aed] text-white px-6 py-2 rounded-full shadow">
+              <Button className="bg-[#7c3aed] text-white hover:bg-purple-800 px-6 py-2 rounded-full shadow">
                 Alterar
               </Button>
             </Link>
